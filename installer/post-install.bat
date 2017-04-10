f322200f4969cf260244f5c22dea4cea7f3f89dc
Hans Permana <hans.permana@gmail.com>
2017-04-10 15:07:50 +0200

    add version info in post install scripts

---
+++ installer/post-install.bat
@@ -0,0 +1,3 @@
+set VERSION=0.5.4
+mkdir "%USERPROFILE%\.dedop\%VERSION%"
+echo %PREFIX% > %USERPROFILE%\.dedop\%VERSION%\dedop.location
---
+++ installer/post-install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+VERSION="0.5.4"
+mkdir -p ${HOME}/.dedop/${VERSION}
+echo ${PREFIX} > ${HOME}/.dedop/${VERSION}/dedop.location
