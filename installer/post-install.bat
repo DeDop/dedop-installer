79481beb8724f99e8f221372e6410939c05f2d6c
Hans Permana <hans.permana@gmail.com>
2018-02-06 15:22:44 +0100

    use python 3.6

--- installer/post-install.bat
+++ installer/post-install.bat
@@ -1,3 +1,3 @@
-set VERSION=1.3.0
+set VERSION=1.4.0.dev1
 mkdir "%USERPROFILE%\.dedop\%VERSION%"
 echo %PREFIX% > %USERPROFILE%\.dedop\%VERSION%\dedop.location
--- installer/post-install.sh
+++ installer/post-install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-VERSION="1.3.0"
+VERSION="1.4.0.dev1"
 mkdir -p ${HOME}/.dedop/${VERSION}
 echo ${PREFIX} > ${HOME}/.dedop/${VERSION}/dedop.location
