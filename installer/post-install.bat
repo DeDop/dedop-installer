14a40301e04ebfd3c17576691de0b93fee8a4196
hans-permana <hans.permana@gmail.com>
2018-08-17 14:11:36 +0200

    version 1.5.0, b1

--- installer/post-install.bat
+++ installer/post-install.bat
@@ -1,3 +1,3 @@
-set VERSION=1.4.0
+set VERSION=1.5.0
 mkdir "%USERPROFILE%\.dedop\%VERSION%"
 echo %PREFIX% > %USERPROFILE%\.dedop\%VERSION%\dedop.location
--- installer/post-install.sh
+++ installer/post-install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-VERSION="1.4.0"
+VERSION="1.5.0"
 mkdir -p ${HOME}/.dedop/${VERSION}
 echo ${PREFIX} > ${HOME}/.dedop/${VERSION}/dedop.location
